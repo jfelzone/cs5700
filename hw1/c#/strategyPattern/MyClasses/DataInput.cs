e60f3619c973ea96525064c2bd3bbb17581d275b
jfelzone <jakefelzien21ati@gmail.com>
2017-09-13 17:54:28 -0600

    saving changes so far just to get it up there

---
+++ hw1/c#/strategyPattern/MyClasses/DataInput.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace MyClasses
+{
+    public abstract class DataInput
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //public abstract void Write(List<ThingABob> list, string filename);
+        //public abstract void Read(List<ThingABob> list, string filename);
+
+       
+    }
+}
---
+++ hw1/c#/strategyPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MyClasses;
+
+namespace strategyPattern
+{
+    class Program
+    {
+        private static readonly DataInput[] DataInputList = new DataInput[]
+                        {
+                            new JsonInput() { Name = "JSON", Description  = "JavaScript Object Notation"},
+                            new XmlInput() { Name = "XML", Description = "Extensible Markup Language"}
+                        };
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DataInputList[0].Name);
+            Console.WriteLine(DataInputList[0].Description);
+            Console.WriteLine(DataInputList[1].Name);
+            Console.WriteLine(DataInputList[1].Description);
+
+            Person bob = new Person(){FirstName = "Hello sicko", LastName = "dummy"};
+            Console.WriteLine(bob.ToString());
+            //Console.WriteLine(DataInputList[0].Check);
+        }
+    }
+}
