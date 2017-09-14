7e0078d576dd4d7fecf1009b176b4f14d33ed53b
Jake Felzien <jakefelzien21ati@gmail.com>
2017-09-14 06:10:06 -0600

    adding all changes for c# for cleanup code, prepping to write unit tests

--- hw1/c#/strategyPattern/MyClasses/XmlInput.cs
+++ hw1/c#/strategyPattern/MyClasses/XmlInput.cs
@@ -1,39 +1,23 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Json;
-using System;
+using System.Xml.Serialization;
 
 namespace MyClasses
 {
     public class XmlInput : DataInput
     {
+        private static readonly XmlSerializer XmlSerializer = new XmlSerializer(typeof(List<Person>),
+                    new [] { typeof(Person) });
           public override void Read(List<Person> list, string filename)
         {
-            //filename = AppendExtension(filename, "json");
-            Console.WriteLine("worked");
+           //filename = AppendExtension(filename, "json");
+            StreamReader reader = new StreamReader(filename);
+            List<Person> data = XmlSerializer.Deserialize(reader.BaseStream) as List<Person>;
+            if (data!=null)
+            {
+                foreach(Person thing in data)
+                    list.Add(thing);
+            }
         }
-        
-        // private static readonly DataContractJsonSerializer JsonSerializer = new DataContractJsonSerializer(typeof(List<ThingABob>),
-        //              new [] { typeof(ThingABob), typeof(Gadget), typeof(Widget) });
-
-        // public override void Write(List<ThingABob> data, string filename)
-        // {
-        //     filename = AppendExtension(filename, "json");
-        //     StreamWriter writer = new StreamWriter(filename);
-        //     JsonSerializer.WriteObject(writer.BaseStream, data);
-        //     writer.Close();
-        // }
-
-        // public override void Read(List<ThingABob> list, string filename)
-        // {
-        //     filename = AppendExtension(filename, "json");
-        //     StreamReader reader = new StreamReader(filename);
-        //     List<ThingABob> data = JsonSerializer.ReadObject(reader.BaseStream) as List<ThingABob>;
-        //     if (data!=null)
-        //     {
-        //         foreach(ThingABob thing in data)
-        //             list.Add(thing);
-        //     }
-        // }
     }
 }
