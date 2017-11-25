cb143256b1b1018cb5130b5c92a1b4694c516709
Jake Felzien <jakefelzien21ati@gmail.com>
2017-11-25 12:16:57 -0700

    adding some more changes

--- hw4/pythonProto/homework4.py
+++ hw4/pythonProto/homework4.py
@@ -63,8 +63,27 @@ class SudukoData:
 
         for x, i in enumerate(self.puzzlearray):
             for y, j in enumerate(self.puzzlearray):
-                print x,y
-
+                #print x , y
+                if x <= 2 and y <= 2:
+                    self.subBoxes[0].append((x,y))
+                elif x <= 5 and x >= 3 and y <=2:
+                    self.subBoxes[1].append((x,y))
+                elif x <= 8 and x >= 6 and y <=2:
+                    self.subBoxes[2].append((x,y))
+                elif x <= 2 and y >= 3 and y <= 5:
+                    self.subBoxes[3].append((x,y))
+                elif x >= 3 and x <= 5 and y >= 3 and y <= 5:
+                    self.subBoxes[4].append((x,y))
+                elif x >= 6 and y >= 3 and y <= 5:
+                    self.subBoxes[5].append((x,y))
+                elif x >= 0 and x <= 2 and y >= 6 and y <= 8:
+                    self.subBoxes[6].append((x,y))
+                elif x >=3 and x <= 5 and y >= 6 and y <= 8:
+                    self.subBoxes[7].append((x,y))
+                elif x >= 6 and x <= 8 and y >= 6 and y <= 8:
+                    self.subBoxes[8].append((x,y))
+        for i in self.subBoxes:
+            print i, self.subBoxes[i]
 
 
 
@@ -77,26 +96,89 @@ class AlgorithmBasis:
 
 
 class RowFillIn(AlgorithmBasis):
-    def __init__(self, puzzle):
+    def __init__(self, puzzle, missingSlot):
         self.puzzle = puzzle
+        self.missingSlot = missingSlot
 
     def applyalgo(self):
+        rowIndex = self.missingSlot[0]
+        print "the rowindex value", rowIndex
         for index1, i in enumerate(self.puzzle.puzzlearray):
-            temp = []
-            for j in i:
-                if j not in temp and j != '-':
-                    temp.append(j)
-            for index2, j in enumerate(i):
-                if j == '-':
-                    possibleVals = Set(self.puzzle.possibleValuesArray)
-                    currentRow = Set(i)
-                    print "length test", len(possibleVals.difference(currentRow))
-                    if len(possibleVals.difference(currentRow)) == 1:
-                        for i in possibleVals.difference(currentRow):
-                            value = i
-                        self.puzzle.puzzlearray[index1][index2] = value
-                    else:
-                        continue
+            if index1 == rowIndex:
+                temp = []
+                for j in i:
+                    if j not in temp and j != '-':
+                        temp.append(j)
+                for index2, j in enumerate(i):
+                    if j == '-':
+                        possibleVals = Set(self.puzzle.possibleValuesArray)
+                        currentRow = Set(i)
+                        print "length test", len(possibleVals.difference(currentRow))
+                        if len(possibleVals.difference(currentRow)) == 1:
+                            for i in possibleVals.difference(currentRow):
+                                value = i
+                            self.puzzle.puzzlearray[index1][index2] = value
+                        else:
+                            continue
+
+class SingleBoxFillIn(AlgorithmBasis):
+    def __init__(self, puzzle, missingSlot):
+        self.puzzle = puzzle
+        self.missingSlot = missingSlot
+        self.boxLoc = None
+
+    def applyalgo(self):
+        for i in self.puzzle.subBoxes:
+            if self.missingSlot in self.puzzle.subBoxes[i]:
+                self.boxLoc = i
+
+        print self.boxLoc
+
+        tempBoxValues = []
+        tempBoxIndexValues = []
+        for x, i in enumerate(self.puzzle.puzzlearray):
+            for y, j in enumerate(i):
+                if (x,y) in self.puzzle.subBoxes[self.boxLoc]:
+                    #print x,y
+                    tempBoxValues.append(j)
+                    tempBoxIndexValues.append((x,y))
+        print tempBoxValues
+        print tempBoxIndexValues
+        #for i in tempBoxValues:
+
+            #print i
+        numberOfEmpties = self.returnNumberOfDashes(tempBoxValues)
+        print numberOfEmpties
+
+
+    def returnNumberOfDashes(self, array):
+        count = 0
+        for i in array:
+            if i == '-':
+                count += 1
+        return count
+
+# class RowFillIn(AlgorithmBasis):
+#     def __init__(self, puzzle):
+#         self.puzzle = puzzle
+#
+#     def applyalgo(self):
+#         for index1, i in enumerate(self.puzzle.puzzlearray):
+#             temp = []
+#             for j in i:
+#                 if j not in temp and j != '-':
+#                     temp.append(j)
+#             for index2, j in enumerate(i):
+#                 if j == '-':
+#                     possibleVals = Set(self.puzzle.possibleValuesArray)
+#                     currentRow = Set(i)
+#                     print "length test", len(possibleVals.difference(currentRow))
+#                     if len(possibleVals.difference(currentRow)) == 1:
+#                         for i in possibleVals.difference(currentRow):
+#                             value = i
+#                         self.puzzle.puzzlearray[index1][index2] = value
+#                     else:
+#                         continue
 
 class ColumnFillIn(AlgorithmBasis):
     def __init__(self, puzzle):
@@ -126,6 +208,8 @@ class FindMisingIndices(AlgorithmBasis):
                     self.storeIndex.append((index1, index2))
 
 
+
+
 # IDEA: i think here i want to do a better job of splitting stuff up. massive for loops is going to get ugly fast
 # IDEA: i also think that it is going to be best to check rows and columns and have a specific thing for that
 # IDEA: i think i want to be able to check stuff like rows and columns independently but im trying to envision the most productful way to do this
@@ -145,6 +229,24 @@ if __name__ == "__main__":
     #testingFile = "SamplePuzzles/Input/Puzzle-4x4-0001.txt"
     testingFile = "SamplePuzzles/Input/Puzzle-9x9-0001.txt"
     testPuzzle = SudukoData(testingFile)
+
+
+    rowFill = RowFillIn(testPuzzle, testPuzzle.missingIndices[0])
+    rowFill.applyalgo()
+
+    boxFill = SingleBoxFillIn(testPuzzle, testPuzzle.missingIndices[0])
+    boxFill.applyalgo()
+
+    # this is the format we are going to want until everything is filled in i believe (something along these lines)
+    # for i in testPuzzle.missingIndices:
+    #     # rowFill.applyalgo()
+    #     # boxFill.applyalgo()
+    #     rowFill = RowFillIn(testPuzzle, i)
+    #     rowFill.applyalgo()
+    #
+    #     boxFill = SingleBoxFillIn(testPuzzle, i)
+    #     boxFill.applyalgo()
+
 
     #store = FindMisingIndices(testPuzzle)
     #store.applyalgo()
